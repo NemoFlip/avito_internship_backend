39c8cfab5dc611185a965242276b8cebe2b071ce
NemoFlip <artgamer3163@gmail.com>
2025-02-18 11:57:59 +0300

    made: custom logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,14 +4,17 @@ import (
 	"avito_internship_backend/config"
 	_ "avito_internship_backend/docs"
 	"avito_internship_backend/internal/app"
+	"avito_internship_backend/pkg/logger"
 	"fmt"
 )
 
 func main() {
+	logging := logger.InitLogger()
 	cfg, err := config.NewConfig()
 	if err != nil {
 		fmt.Println(err)
 	}
+	logging.InfoLogger.Info("Cfg initialized")
 
 	app.Run(cfg)
 	// TODO: initialize ogger - then continue in internal/app/app.go
---
+++ pkg/logger/logger.go
@@ -0,0 +1,52 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+type Logger struct {
+	InfoLogger  *logrus.Logger
+	ErrorLogger *logrus.Logger
+}
+
+func setConfiguration(logger *logrus.Logger) {
+	logger.SetReportCaller(true)
+	logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "02.01.2006 15:04:05",
+		CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
+			dir, fileName := filepath.Split(f.File)
+			dir = strings.TrimSuffix(dir, string(os.PathSeparator)) // reduce possible slash in the end of dirname
+			lastDir := path.Base(dir)
+			file = fmt.Sprintf("%s/%s:%d", lastDir, fileName, f.Line)
+			return f.Function, file
+		},
+	})
+
+}
+
+func InitLogger() *Logger {
+	infoLogger := logrus.New()
+	errorLogger := logrus.New()
+	setConfiguration(infoLogger)
+	setConfiguration(errorLogger)
+	infoLoggerFile, err := os.OpenFile("cmd/logs/logs.info", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0660)
+	if err != nil {
+		panic("unable to open info file for logs")
+	}
+	errorLoggerFile, err := os.OpenFile("cmd/logs/logs.error", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0660)
+	if err != nil {
+		panic("unable to open error file for logs")
+	}
+	infoLogger.Out = infoLoggerFile
+	errorLogger.Out = errorLoggerFile
+	return &Logger{
+		InfoLogger:  infoLogger,
+		ErrorLogger: errorLogger,
+	}
+}
